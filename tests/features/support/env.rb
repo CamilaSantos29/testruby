f53761def27de95a75a9aca8a9ea099430b3aa11
camila2911 <kmillaana29@gmail.com>
2022-05-19 11:44:46 -0300

    again

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -4,16 +4,28 @@ require 'site_prism'
 require_relative 'helper.rb'
 require_relative 'page_helper.rb'
 
+BROWSER = ENV['BROWSER']
 AMBIENTE = ENV['AMBIENTE']
 
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
 
-
 World(Helper)
 World(Pages)
 
+Capybara.register_driver :selenium do |app|
+
+    if BROWSER.eql?('chrome')
+      Capybara::Selenium::Driver.new(app, :browser => :chrome)
+      elsif BROWSER.eql?('chrome_headless')
+      Capybara::Selenium::Driver.new(app, :browser => :chrome,
+        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+          'goog:chromeOptions' => {'args'=>['--headless', 'disable-gpu']}
+        ))
+        end
+    end
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium_chrome_headless
     config.app_host = CONFIG['url_padrao']
     config.default_max_wait_time = 10
 end
